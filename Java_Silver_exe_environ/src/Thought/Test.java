4257fde23dd2cd2e6cf6240e047f7f27229212a9
kki25 <kki25@host.docker.internal>
2022-08-08 21:29:16 +0900

    first commit

---
+++ Java_Silver_exe_environ/src/Thought/Test.java
@@ -0,0 +1,11 @@
+package Thought;
+
+public class Test {
+	public static void main(String[] args) {
+//		System.out.println("Hello");
+		String str = "abc";
+		String str2 = str.replace("a", "b");
+		System.out.println(str);
+		System.out.println(str2);
+	}
+}
---
+++ Java_Silver_exe_environ/src/commentary/Test.java
@@ -0,0 +1,9 @@
+package commentary;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+
+}
